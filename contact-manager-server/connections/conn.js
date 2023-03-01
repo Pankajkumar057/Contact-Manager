e11f2c0b2c7fd964d745ca1a05cf41c25ba27f22
shivangi <shivangikris123@gmail.com>
2023-03-01 12:53:19 +0530

    connected to db

--- contact-manager-server/connections/conn.js
+++ contact-manager-server/connections/conn.js
@@ -5,7 +5,7 @@ const dbconnect = async ()=>{
     mongoose.connect(process.env.DB_URI,{ useNewUrlParser: true });
     mongoose.connection.on('connected',()=> console.log("Database Connected!!!"));
     mongoose.connection.on('error',(e)=>console.log(e));
-    mongoose.connection.on('disconnected',()=>console.log("------------Database Disconnected---------"))
+    mongoose.connection.on('disconnected',()=>console.log("_________Database Disconnected__________"))
 }
 
 module.exports= dbconnect;

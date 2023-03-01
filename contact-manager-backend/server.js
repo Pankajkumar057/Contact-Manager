763c7b5b663a108476fd30d2431f67513ee49918
Navaraj <paripalli.navaraj18@gmail.com>
2023-03-01 18:27:37 +0530

    implemented Register and login route

--- contact-manager-backend/server.js
+++ contact-manager-backend/server.js
@@ -1,7 +1,13 @@
 const express=require("express");
 const connectDB =require("./config/db.js");
+const cors=require('cors');
+const bodyParser=require('body-parser')
+
 const dotenv=require("dotenv")
-const Contact=require("./models/contactModel")
+
+const registerAndLogin = require('./routes/RegisterAndLogin');
+
+
 
 
 dotenv.config();
@@ -9,9 +15,11 @@ const app=express();
 connectDB()
 
 const PORT=process.env.Port || 3000
+app.use(cors)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(registerAndLogin);
 
- app.get("/",(req,res)=>{
-    res.end("Hello world")
- })
+
 
  app.listen(PORT,console.log(`server running in ${PORT} port`))
